f03886334069344d8296752536c3fc82e5d4d38a
Dawid Majewski <majewskicisco@gmail.com>
2019-03-02 20:50:29 +0100

    Updates

---
+++ precache-manifest.5a86611e83e57738e23e9d5a1efd56ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c561285c8cb13537b524",
+    "url": "/coffee_mug/static/js/runtime~main.c561285c.js"
+  },
+  {
+    "revision": "e39b2685115a06be2961",
+    "url": "/coffee_mug/static/js/main.e39b2685.chunk.js"
+  },
+  {
+    "revision": "3429f784f74d6ef6fefa",
+    "url": "/coffee_mug/static/js/2.3429f784.chunk.js"
+  },
+  {
+    "revision": "e39b2685115a06be2961",
+    "url": "/coffee_mug/static/css/main.62de00b7.chunk.css"
+  },
+  {
+    "revision": "dd1e2d5e64b44ad3c4e99e562e33ffa8",
+    "url": "/coffee_mug/index.html"
+  }
+];
